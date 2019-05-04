25c3e090ed78b8cef2eedded86c5d68678109469
punk <smxpro@163.com>
2019-05-04 14:04:23 +0800

    add utils

--- app/src/main/java/com/bnb/pursue/MainActivity.java
+++ app/src/main/java/com/bnb/pursue/MainActivity.java
@@ -1,11 +1,7 @@
 package com.bnb.pursue;
 
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,22 +9,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
         initListener();
     }
 
     private void initListener() {
         findViewById(R.id.tv_test).setOnClickListener(v -> {
-            MyUpdateHelper helper = new MyUpdateHelper(MainActivity.this);
-            helper.startUpdate();
         });
-    }
-
-    private void init() {
-        String printTxtPath = getApplicationContext().getPackageResourcePath() + "/files/";
-        File filesDir = getFilesDir();
-        File dataDirectory = Environment.getDataDirectory();
-
-        Log.e("asd", "path -> " + dataDirectory.getAbsolutePath());
     }
 }
--- wbasemodule/src/main/java/com/bnb/wbasemodule/utils/SpHelper.java
+++ wbasemodule/src/main/java/com/bnb/wbasemodule/utils/SpHelper.java
@@ -1,25 +1,67 @@
 package com.bnb.wbasemodule.utils;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class SpHelper {
 
-    private static SpHelper sInstance;
+    public static void put(Context context, String key, Object object) {
+        SharedPreferences sp = context.getSharedPreferences("W_SP_DATA",
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
 
-    private SpHelper() {
-
-    }
-
-    public static SpHelper getInstance() {
-        if (sInstance == null) {
-            synchronized (SpHelper.class) {
-                if (sInstance == null) {
-                    sInstance = new SpHelper();
-                }
-            }
+        if (object instanceof String) {
+            editor.putString(key, (String) object);
+        } else if (object instanceof Integer) {
+            editor.putInt(key, (Integer) object);
+        } else if (object instanceof Boolean) {
+            editor.putBoolean(key, (Boolean) object);
+        } else if (object instanceof Float) {
+            editor.putFloat(key, (Float) object);
+        } else if (object instanceof Long) {
+            editor.putLong(key, (Long) object);
+        } else {
+            editor.putString(key, object.toString());
         }
-        return sInstance;
+        editor.apply();
     }
 
-    public void saveStr() {
 
+    public static Object get(Context context, String key, Object defaultObject) {
+        SharedPreferences sp = context.getSharedPreferences("W_SP_DATA",
+                Context.MODE_PRIVATE);
+
+        if (defaultObject instanceof String) {
+            return sp.getString(key, (String) defaultObject);
+        } else if (defaultObject instanceof Integer) {
+            return sp.getInt(key, (Integer) defaultObject);
+        } else if (defaultObject instanceof Boolean) {
+            return sp.getBoolean(key, (Boolean) defaultObject);
+        } else if (defaultObject instanceof Float) {
+            return sp.getFloat(key, (Float) defaultObject);
+        } else if (defaultObject instanceof Long) {
+            return sp.getLong(key, (Long) defaultObject);
+        }
+
+        return null;
     }
+
+
+    public static void remove(Context context, String key) {
+        SharedPreferences sp = context.getSharedPreferences("W_SP_DATA",
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.remove(key);
+        editor.apply();
+    }
+
+
+    public static void clear(Context context) {
+        SharedPreferences sp = context.getSharedPreferences("W_SP_DATA",
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.clear();
+        editor.apply();
+    }
+
 }
